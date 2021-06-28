b3a3777f97897c8b9b995f98bf081f5cbc6b85e9
AndrewSimons3 <simdrew07@gmail.com>
2021-06-28 18:47:43 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ReservationModal from './Components/ReservationModal';
+import Navbar from './Components/Navbar';
+
 
 function App() {
+
+  const [ReservationIsShown, setReservationIsShown] = useState(false);    
+
+  const showReservationHandler = () => {
+    setReservationIsShown(true);
+  }
+
+  const hideReservationHandler = () => {
+    setReservationIsShown(false);
+  } 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {ReservationIsShown && <ReservationModal onClose={hideReservationHandler} />}
+     <Navbar onShowReservation={showReservationHandler}/>
+     <main>
+
+     </main>
     </div>
   );
 }
